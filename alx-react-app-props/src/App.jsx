23f91badbdc271c6ee0624e2aee746d7d95df13a
Ernest Awuku Junior <awukuernest@gmail.com>
2025-07-20 12:55:17 +0000

    Move UserContext to components directory
    Renamed UserContext.js and relocated it from the src root to the components directory. Updated import path in App.jsx to reflect the new location for better project structure.

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header';
 import './App.css'
 import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
-import UserContext from './UserContext';
+import UserContext from './components/UserContext';
 
 function App() {
 

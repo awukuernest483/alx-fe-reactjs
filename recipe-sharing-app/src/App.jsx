611621415f3cb6d39438afedf77db78b35402ae8
Ernest Awuku Junior <awukuernest@gmail.com>
2025-07-27 21:51:15 +0000

    Add favorites and recommendations features
    Introduced FavoritesList and RecommendationsList components to display user's favorite recipes and recommended recipes. Updated recipeStore to manage favorites and recommendations, including methods to add/remove favorites and generate recommendations. App.jsx now renders the new components.

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,15 +4,18 @@ import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeDetails from './components/RecipeDetails';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Router>
       <div>
         <RecipeList/>
         <AddRecipeForm />
+        <FavoritesList />
+        <RecommendationsList />
       </div>
       <Routes>
         <Route path="/recipes/:recipeId" element={<RecipeDetailsWrapper />} />

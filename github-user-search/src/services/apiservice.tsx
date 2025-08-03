88f6737d713198b5545c5561039a0bb34c7ebb78
Ernest Awuku Junior <awukuernest@gmail.com>
2025-08-03 16:57:11 +0000

    chnages

--- github-user-search/src/services/apiservice.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const apiservice = () => {
-  return <div>apiservice</div>;
-};
-
-export default apiservice;
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,7 @@
 const fetchUserData = async (username) => {
   const response = await fetch(`https://api.github.com/users/${username}`);
   if (!response.ok) {
-    throw new Error("User not found");
+    throw new Error("Looks like we cant find the user");
   }
   const data = await response.json();
   return data;

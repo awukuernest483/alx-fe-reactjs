3b8bfebf8ec5e186959a5065185721cafcd9b06a
Ernest Awuku Junior <awukuernest@gmail.com>
2025-08-17 19:13:38 +0000

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Homepage from './components/Homepage'
+import Homepage from './components/HomePage'
 import React from "react";
 import ReactDOM from "react-dom/client";
 
